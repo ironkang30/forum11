7cd31a87f595c8bf75a8f1c74ada1495df8fa39d
ironkang30 <ironkang30@gmail.com>
2025-09-07 14:01:17 +0900

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['Arial', 'Helvetica', 'sans-serif'],
+      },
+      animation: {
+        'gradient': 'gradient 3s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-position': '0% 50%',
+          },
+          '50%': {
+            'background-position': '100% 50%',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
